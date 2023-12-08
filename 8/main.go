88d5a97aad9dcc03feda414ee567239c310c2172
Stephen Johnston <tiquortoo@gmail.com>
2023-12-08 19:17:44 +0000

    Add parallel solve function

--- 8/main.go
+++ 8/main.go
@@ -13,6 +13,12 @@ type inputNode struct {
 	right string
 }
 
+type inputValuesParallel struct {
+	start      string
+	directions []string
+	nodes      map[string]inputNode
+}
+
 var target string = "ZZZ"
 
 func main() {
@@ -22,6 +28,54 @@ func main() {
 }
 
 func solve(input inputValues) int {
+	fmt.Printf("Input: %+v\n", input.directions)
+
+	// for key, nodes := range input.nodes {
+	// 	fmt.Printf("Key -%+v- Node %+v\n", key, nodes)
+	// }
+
+	steps := 0
+	nextNodeKey := input.start
+	currentNode := input.nodes[nextNodeKey]
+	found := false
+	iterations := 0
+	for !found {
+		directionsIndex := 0
+		//fmt.Print("Still Not Found \n")
+		for directionsIndex < len(input.directions) {
+			currentNodeKey := nextNodeKey
+			//fmt.Printf("Node: %+v\n", nextNode)
+			direction := input.directions[directionsIndex]
+			if direction == "L" {
+				nextNodeKey = currentNode.left
+				//fmt.Printf("left: %s\n", nextNodeKey)
+			} else if direction == "R" {
+				nextNodeKey = currentNode.right
+				//fmt.Printf("right: %s\n", nextNodeKey)
+			} else {
+				//fmt.Printf("Unknown Direction: %s\n", direction)
+				return 0
+			}
+			steps++
+			fmt.Printf("Step: %d Current Node Key: -%s- Current Node: %+v Direction: -%s- Next Node Key: -%s-\n", steps, currentNodeKey, currentNode, direction, nextNodeKey)
+			if nextNodeKey == target {
+				fmt.Print("Found \n")
+				found = true
+				break
+			}
+			currentNode = input.nodes[nextNodeKey]
+			// fmt.Printf("Moving: %s to %+v\n", direction, nextNode)
+			// fmt.Printf("Input: %+v\n", input.nodes)
+			directionsIndex++
+		}
+		iterations++
+	}
+
+	fmt.Printf("Target: %s Reached In: %d Steps\n", target, steps)
+	return steps
+}
+
+func solveParalell(input inputValues) int {
 	fmt.Printf("Input: %+v\n", input.directions)
 
 	// for key, nodes := range input.nodes {
