0005be1942951c871770bab1179a5c8e14fb7bbe
Stephen Johnston <tiquortoo@gmail.com>
2023-12-08 20:07:56 +0000

    Add parallel solving functionality

--- 8/main.go
+++ 8/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 type inputValues struct {
 	start      string
@@ -14,12 +17,13 @@ type inputNode struct {
 }
 
 type inputValuesParallel struct {
-	start      string
+	start      []string
 	directions []string
 	nodes      map[string]inputNode
 }
 
 var target string = "ZZZ"
+var parallelTarget string = "Z"
 
 func main() {
 	fmt.Printf("No Real Main Day 8")
@@ -75,15 +79,44 @@ func solve(input inputValues) int {
 	return steps
 }
 
-func solveParalell(input inputValues) int {
-	fmt.Printf("Input: %+v\n", input.directions)
+type foundNode struct {
+	steps       int
+	nextNodeKey string
+}
 
+func solveParalell(input inputValuesParallel) int {
+	fmt.Printf("Input: %+v\n", input)
+
+	var wg sync.WaitGroup
+
+	steps := 0
+	found := false
+	for !found {
+		for i := 0; i < len(input.start); i++ {
+			wg.Add(1)
+			fmt.Printf("=============== Running: %s ================== \n", input.start[i])
+			go func(ele string) {
+				defer wg.Done()
+				// TODO: Figure out return values from waitgroups - WORTH Working on Beyond Competition
+				//foundNode := runStep(input, ele)
+				runStep(input, ele)
+			}(input.start[i])
+		}
+		wg.Wait()
+
+		found = true
+	}
+
+	fmt.Printf("Target: %s Reached In: %d Steps\n", target, steps)
+	return steps
+}
+
+func runStep(input inputValuesParallel, nextNodeKey string) foundNode {
 	// for key, nodes := range input.nodes {
 	// 	fmt.Printf("Key -%+v- Node %+v\n", key, nodes)
 	// }
 
 	steps := 0
-	nextNodeKey := input.start
 	currentNode := input.nodes[nextNodeKey]
 	found := false
 	iterations := 0
@@ -102,11 +135,15 @@ func solveParalell(input inputValues) int {
 				//fmt.Printf("right: %s\n", nextNodeKey)
 			} else {
 				//fmt.Printf("Unknown Direction: %s\n", direction)
-				return 0
+				return foundNode{}
 			}
 			steps++
 			fmt.Printf("Step: %d Current Node Key: -%s- Current Node: %+v Direction: -%s- Next Node Key: -%s-\n", steps, currentNodeKey, currentNode, direction, nextNodeKey)
-			if nextNodeKey == target {
+			// trimmedItemKey := strings.Trim(linePieces[0], " ")
+			// if string(trimmedItemKey[len(trimmedItemKey)-1:][0]) == "A" {
+			// 	start = append(start, trimmedItemKey)
+			// }
+			if string(nextNodeKey[len(nextNodeKey)-1:][0]) == parallelTarget {
 				fmt.Print("Found \n")
 				found = true
 				break
@@ -117,8 +154,10 @@ func solveParalell(input inputValues) int {
 			directionsIndex++
 		}
 		iterations++
+		// if iterations > 1 {
+		// 	break
+		// }
 	}
 
-	fmt.Printf("Target: %s Reached In: %d Steps\n", target, steps)
-	return steps
+	return foundNode{steps: steps, nextNodeKey: nextNodeKey}
 }
--- 8/main_test.go
+++ 8/main_test.go
@@ -31,17 +31,17 @@ func TestKnownSolve2(t *testing.T) {
 	}
 }
 
-func TestUnknownSolve(t *testing.T) {
-	input, err := loadInput("input.txt")
-	if err != nil {
-		t.Error(err)
-	}
-	expected := 0
-	result := solve(input)
-	if result != expected {
-		t.Errorf("%s = %d; want %d", "input.txt", result, expected)
-	}
-}
+// func TestUnknownSolve(t *testing.T) {
+// 	input, err := loadInput("input.txt")
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// 	expected := 0
+// 	result := solve(input)
+// 	if result != expected {
+// 		t.Errorf("%s = %d; want %d", "input.txt", result, expected)
+// 	}
+// }
 
 func loadInput(loadLocation string) (inputValues, error) {
 	file, err := os.Open(loadLocation)
@@ -92,6 +92,90 @@ func loadInput(loadLocation string) (inputValues, error) {
 	// }
 
 	loadedValues := inputValues{start: start, directions: directionsArray, nodes: nodes}
+
+	// fmt.Printf("Loaded \n%+v\n", loadedValues)
+
+	return loadedValues, nil
+}
+
+func TestKnownParallel(t *testing.T) {
+	input, err := loadParellelInput("known_parallel.txt")
+	if err != nil {
+		t.Error(err)
+	}
+	expected := 0
+	result := solveParalell(input)
+	if result != expected {
+		t.Errorf("%s = %d; want %d", "input.txt", result, expected)
+	}
+}
+
+// func TestUnknownParallel(t *testing.T) {
+// 	input, err := loadParellelInput("input.txt")
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// 	expected := 0
+// 	result := solveParalell(input)
+// 	if result != expected {
+// 		t.Errorf("%s = %d; want %d", "input.txt", result, expected)
+// 	}
+// }
+
+func loadParellelInput(loadLocation string) (inputValuesParallel, error) {
+	file, err := os.Open(loadLocation)
+	if err != nil {
+		return inputValuesParallel{}, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	directions := scanner.Text()
+
+	var directionsArray []string
+	for _, byte := range directions {
+		directionsArray = append(directionsArray, string(byte))
+	}
+
+	var nodes = make(map[string]inputNode)
+	var start []string // not always A, we're going to have to find them!
+	for scanner.Scan() {
+		// skip blanks
+		line := scanner.Text()
+		if line != "" {
+			//nodes = append(nodes, strings.TrimSpace(scanner.Text()))
+			linePieces := strings.Split(line, "=")
+			lineNodes := strings.Trim(linePieces[1], " ")
+			lineNodes = strings.Trim(lineNodes, "(")
+			lineNodes = strings.Trim(lineNodes, ")")
+			nodeElements := strings.Split(lineNodes, ", ")
+
+			inputNode := inputNode{left: nodeElements[0], right: nodeElements[1]}
+			nodes[strings.Trim(linePieces[0], " ")] = inputNode
+
+			trimmedItemKey := strings.Trim(linePieces[0], " ")
+			if string(trimmedItemKey[len(trimmedItemKey)-1:][0]) == "A" {
+				start = append(start, trimmedItemKey)
+			}
+			// START IS ALWAYS AAA
+			// if start == "" {
+			// 	start = strings.Trim(linePieces[0], " ")
+			// }
+			//nodes = append(nodes, inputNode)
+		}
+	}
+
+	// verify their shape
+	// for _, direction := range directions {
+	// 	fmt.Printf("-%+v-\n", direction)
+	// }
+
+	// for _, node := range nodes {
+	// 	fmt.Printf("-%+v-\n", node)
+	// }
+
+	loadedValues := inputValuesParallel{start: start, directions: directionsArray, nodes: nodes}
 
 	// fmt.Printf("Loaded \n%+v\n", loadedValues)
 
