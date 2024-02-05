f0c69aeea5427594f0572f1e4f5354c7fd0a06e8
Stephen Johnston <tiquortoo@gmail.com>
2024-02-05 13:50:16 +0000

    2015 day 6 solution

--- 2015/6/main.go
+++ 2015/6/main.go
@@ -43,7 +43,7 @@ func solve(input []string) (int, error) {
 		}
 	}
 
-	return totalOn, fmt.Errorf("not implemented")
+	return totalOn, nil
 }
 
 func applyCommand(command int, value int) int {
@@ -66,6 +66,15 @@ func initPixelArray() [][]int {
 	for i := range pixels {
 		pixels[i] = make([]int, 1000)
 	}
+
+	for x := 0; x <= 999; x++ {
+		for y := 0; y <= 999; y++ {
+			if pixels[x][y] == 1 {
+				pixels[x][y] = 0
+			}
+		}
+	}
+
 	return pixels
 }
 
@@ -95,4 +104,46 @@ func getInstruction(input string) instruction {
 	instruction.endY, _ = strconv.Atoi(end[1])
 
 	return instruction
+}
+
+func solveBonus(input []string) (int, error) {
+	pixels := initPixelArray()
+
+	for _, instructionLine := range input {
+		//fmt.Printf("Instruction: %s ", instructionLine)
+		instruction := getInstruction(instructionLine)
+		//fmt.Printf("Instruction: %+v %s\n", instruction, instructionLine)
+		for x := instruction.startX; x <= instruction.endX; x++ {
+			for y := instruction.startY; y <= instruction.endY; y++ {
+				pixels[x][y] = pixels[x][y] + applyBonusCommand(instruction.command, pixels[x][y])
+				//fmt.Printf(" %d\n", pixels[x][y])
+			}
+			//fmt.Println()
+		}
+	}
+
+	totalOn := 0
+
+	for x := 0; x <= 999; x++ {
+		for y := 0; y <= 999; y++ {
+			totalOn = totalOn + pixels[x][y]
+		}
+	}
+
+	return totalOn, nil
+}
+
+func applyBonusCommand(command int, value int) int {
+	//fmt.Printf("Command: %d", command)
+	if command == 1 { // on
+		return 1
+	} else if command == 0 { // off
+		if value > 0 {
+			return -1
+		} else {
+			return 0
+		}
+	} else { // toggle
+		return 2
+	}
 }
--- 2015/6/main_test.go
+++ 2015/6/main_test.go
@@ -37,14 +37,43 @@ func TestGiven(t *testing.T) {
 
 	count, _ = solve([]string{"toggle 0,0 through 999,0"})
 	assert.Equal(t, 1000, count)
+
+	count, _ = solve([]string{"turn on 0,0 through 999,999", "toggle 0,0 through 999,0"})
+	assert.Equal(t, 999000, count)
+
+	count, _ = solve([]string{"turn on 0,0 through 0,9", "toggle 0,0 through 0,4", "turn off 0,0 through 0,1"})
+	assert.Equal(t, 5, count)
+
+	count, _ = solve([]string{"turn on 0,0 through 1,9", "toggle 0,0 through 0,4", "turn off 0,0 through 0,1"})
+	assert.Equal(t, 15, count)
+
+	count, _ = solve([]string{"turn on 0,0 through 1,9", "turn off 0,0 through 1,9"})
+	assert.Equal(t, 0, count)
 }
 
-// func TestSolve(t *testing.T) {
-// 	count, err := solve()
+func TestSolve(t *testing.T) {
+	input, _ := loadInput()
+	count, err := solve(input)
 
-// 	assert.Nil(t, err)
-// 	assert.Equal(t, 0, count)
-// }
+	assert.Nil(t, err)
+	assert.Equal(t, 400410, count)
+}
+
+func TestBonusGiven(t *testing.T) {
+	count, _ := solveBonus([]string{"turn on 0,0 through 0,0"})
+	assert.Equal(t, 1, count)
+
+	count, _ = solveBonus([]string{"toggle 0,0 through 999,999"})
+	assert.Equal(t, 2000000, count)
+}
+
+func TestSolveBonus(t *testing.T) {
+	input, _ := loadInput()
+	count, err := solveBonus(input)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 0, count)
+}
 
 func loadInput() ([]string, error) {
 	results := make([]string, 0)
